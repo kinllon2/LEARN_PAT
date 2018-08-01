c9a6f485ec8d43eacc6b7e9c40496abd26009213
WangChengtao <821997225@qq.com>
2018-08-01 16:25:47 +0800

    [*] fix some error about the Bubble_sort and at this time ,bubble sort can run correctly

--- Arithmetic/Bubble_sort/Bubble_sort/Bubble_sort/Bubble_sort.cpp
+++ Arithmetic/Bubble_sort/Bubble_sort/Bubble_sort/Bubble_sort.cpp
@@ -10,18 +10,27 @@ int main()
 	//存入需要排序的数据
 	vector<int> VecInt;
 	int num = 0;
-	while (cin.get() != '\r')
+	while (cin.get() != '\n')
 	{
 		cin.unget();
 		cin >> num;
 		VecInt.push_back(num);
 	}
 
-
-
-
-	
-
-
-	return 0;
+	int Slength = VecInt.size();
+	int Index = Slength - 1;
+	int sum = 0;
+	for (int i = 0; i < Index; i++)
+	{
+		for (int j = 0; j < Index - i; j++)
+		{
+			if (VecInt[j] > VecInt[j + 1])
+			{
+				int temp = VecInt[j+1];
+				VecInt[j+1] = VecInt[j];
+				VecInt[j] = temp;
+			}
+		}
+	}
+   	return 0;
 }
