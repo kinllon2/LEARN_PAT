c6d96119da9c62f6991aac1fc9c1950b89a5685a
WangChengtao <821997225@qq.com>
2018-08-01 22:57:06 +0800

    [*] add a function about half insertion_sort

--- Arithmetic/Sort/Insertion_sort/1.Insertion/Insertion/Insertion_sort.cpp
+++ Arithmetic/Sort/Insertion_sort/1.Insertion/Insertion/Insertion_sort.cpp
@@ -3,21 +3,15 @@
 #include <vector>
 
 using namespace std;
+vector<int> VecInt;
 //插排
 //时间复杂度  O（n2）
-int main()
+
+//简单的插入排序
+void Sort1()
 {
-	vector<int> VecInt ;
-	while (cin.get() != '\n')
-	{
-		cin.unget();
-		int number = 0;
-		cin >> number;
-		VecInt.push_back(number);
-	}
-	
 	int slength = VecInt.size();
-	if (slength > 1) 
+	if (slength > 1)
 	{
 		for (int k = 1; k<slength; k++)
 		{
@@ -32,6 +26,56 @@ int main()
 		}
 
 	}
+}
+
+//改进后的插入排序--二分插入排序
+void Sort2()
+{
+	int slength = VecInt.size();
+	for (int i = 1; i < slength - 1; i++)
+	{
+		int get = VecInt[i];
+		int left = 0;
+		int right = i - 1;
+		//从有序集合中找到数字的区间
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (VecInt[mid] > get)
+				right = mid - 1;
+			else
+				left = mid + 1;
+		} 
+		//比目标数字大的数字右移，然后替换left指定的位置
+		for (int j = i - 1; j >= left; j--)
+		{
+			VecInt[j + 1] = VecInt[j];
+		}
+		VecInt[left] = get;
+	}
+
+}
+
+//改进后的插入排序--希尔排序
+void Sort3()
+{
+
+}
+
+int main()
+{
+	
+	while (cin.get() != '\n')
+	{
+		cin.unget();
+		int number = 0;
+		cin >> number;
+		VecInt.push_back(number);
+	}
+	
+	//Sort1();
+	Sort2();
+
 	//VecInt.push_back(5);
 	//int i =VecInt.size();
 	//int j = VecInt[0];
