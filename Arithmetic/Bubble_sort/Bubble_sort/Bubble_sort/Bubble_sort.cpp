3a06123ea9332621515a98c1bb9d902644a55997
WangChengtao <821997225@qq.com>
2018-08-01 17:33:12 +0800

    add a improved bubble-sort in function Sort2

--- Arithmetic/Bubble_sort/Bubble_sort/Bubble_sort/Bubble_sort.cpp
+++ Arithmetic/Bubble_sort/Bubble_sort/Bubble_sort/Bubble_sort.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 vector<int> VecInt;
 
-void sort1()
+
+//使用简单的冒泡排序
+void Sort1()
 {
 	int Slength = VecInt.size();
 	int Index = Slength - 1;
@@ -21,10 +23,41 @@ void sort1()
 			}
 		}
 	}
-
 }
 
 
+//改进冒泡算法，一次循环两边进行冒泡处理*（鸡尾酒排序）
+void Sort2()
+{
+	int Slength = VecInt.size();
+	int Index = Slength - 1;
+	int left = 0;
+	int right = Index;
+	while (left < right)
+	{
+		for (int i = left; i < right; i++)
+		{
+			if (VecInt[i] > VecInt[i + 1])
+			{
+				int temp1 = VecInt[i];
+				VecInt[i] = VecInt[i + 1];
+				VecInt[i + 1] = temp1;
+			}
+		}
+		right--;
+		for (int j = right; j > left; j--)
+		{
+			if (VecInt[j] < VecInt[j-1])
+			{
+				int temp2 = VecInt[j-1];
+				VecInt[j-1] = VecInt[j];
+				VecInt[j] = temp2;
+			}
+		}
+		left++;
+
+	}
+}
 
 
 int main()
@@ -40,7 +73,7 @@ int main()
 		VecInt.push_back(num);
 	}
 
-	sort1();
-	
+	//Sort1();
+	Sort2();
    	return 0;
 }
