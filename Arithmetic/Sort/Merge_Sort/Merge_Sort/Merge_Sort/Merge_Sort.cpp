b9deb045d367cf56e53c97fedd4c95b2e1a43c36
WangChengtao <821997225@qq.com>
2018-08-02 14:20:22 +0800

    [*] ad  a file about merge sort

---
+++ Arithmetic/Sort/Merge_Sort/Merge_Sort/Merge_Sort/Merge_Sort.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> VecInt;
+vector<int> TempVec;
+
+
+//合并操作
+void Merge()
+{
+
+}
+
+//递归方法
+void MergeSortRecursion()
+{
+    
+}
+
+//非递归方法
+void MergeSortRecursion()
+{
+	  
+}
+
+
+
+int main()
+{
+	//input number
+	int num = 0;
+	while(cin.get()!='\n')
+	{
+		cin.unget();
+		cin >> num;
+		VecInt.push_back(num);
+	}
+	//递归实现归并算法
+
+
+	//非递归实现归并算法
+
+	return 0;
+}
