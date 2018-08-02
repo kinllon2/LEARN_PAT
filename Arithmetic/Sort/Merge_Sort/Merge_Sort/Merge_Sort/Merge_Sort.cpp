7bbe78d527d38a0058c447a9b978906ef705cb3c
WangChengtao <821997225@qq.com>
2018-08-02 16:20:41 +0800

    [*]  add a new sort about merge Sort

--- Arithmetic/Sort/Merge_Sort/Merge_Sort/Merge_Sort/Merge_Sort.cpp
+++ Arithmetic/Sort/Merge_Sort/Merge_Sort/Merge_Sort/Merge_Sort.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -9,15 +10,45 @@ vector<int> TempVec;
 
 
 //合并操作
-void Merge()
+void Merge(vector<int> &vec1,int left,int mid,int right)
 {
+	int len = right - left+1;
+	vector<int> temp;
+	int index = 0;
+	int i = left;
+	int j = mid+1;
+	while (i <= mid && j <= right)
+	{
+		int tempnum = 0;
+		tempnum=vec1[i] <= vec1[j] ? vec1[i++] : vec1[j++];
+		temp.push_back(tempnum);
+	}
+	while (i <= mid)
+	{
+		temp.push_back(vec1[i++]);
+	}
+	while (j <= right)
+	{
+		temp.push_back(vec1[j++]);
+	}
+	for (int k = 0; k < len; k++)
+	{
+		vec1[left++]=temp[k];
+	}
+
 
 }
 
 //递归方法
-void MergeSortRecursion()
+void MergeSortRecursion(vector<int> &vec1,int left,int right)
 {
-    
+	if (left >= right)
+		return;
+	int mid = (left + right) / 2;
+	MergeSortRecursion(vec1, left, mid);
+	MergeSortRecursion(vec1, mid + 1, right);
+	Merge(vec1,left,mid,right);
+
 }
 
 //非递归方法
@@ -30,6 +61,7 @@ void MergeSortRecursion()
 
 int main()
 {
+	clock_t t =clock();
 	//input number
 	int num = 0;
 	while(cin.get()!='\n')
@@ -39,9 +71,10 @@ int main()
 		VecInt.push_back(num);
 	}
 	//递归实现归并算法
-
+	MergeSortRecursion(VecInt,0,VecInt.size()-1);
 
 	//非递归实现归并算法
-
+	//clock_t t2 = clock();
+	//clock_t t3 = t2 - t;
 	return 0;
 }
