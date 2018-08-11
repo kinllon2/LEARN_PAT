264eddf0883722b3d512ab6c6a43fb93ad087f6c
WangChengtao <821997225@qq.com>
2018-08-11 14:02:00 +0800

    add a file named 1003 Emergency (25)

---
+++ PAT_Test/1003 Emergency (25)/1003 Emergency (25)/main.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#define INF 100;
+
+using namespace std;
+
+int main()
+{
+	//input 
+	int N = 0;//(N<=500) name of cities
+	int M = 0;//the number of roads
+	int C1,C2 = 0;//the cities that you are currently in and that you must save, respectively.
+
+
+
+
+	return 0;
+}
