0859310f74194bf5959387b51745b3c1d71153ea
WangChengtao <821997225@qq.com>
2018-08-15 20:36:36 +0800

    [*]  add a main 2 and delete to recode

--- PAT_Test/1003 Emergency (25)/1003 Emergency (25)/main.cpp
+++ PAT_Test/1003 Emergency (25)/1003 Emergency (25)/main.cpp
@@ -1,19 +1,6 @@
-#include <stdlib.h>
-#include <iostream>
-#include <iomanip>
-#define INF 100;
+#include<stdlib.h>
+#include<vector>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
-	//input 
-	int N = 0;//(N<=500) name of cities
-	int M = 0;//the number of roads
-	int C1,C2 = 0;//the cities that you are currently in and that you must save, respectively.
-
-
-
-
-	return 0;
-}
