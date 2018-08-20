ee8f31ec00b5d66a3bcb5659272c5c50647057a2
WangChengtao <821997225@qq.com>
2018-08-20 22:01:14 +0800

    start 1012

---
+++ PAT_Test/1012 The Best Rank (25)/The_Best_Rank/The_Best_Rank/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+#include<vector>
+
+using namespace std;
+
+struct student
+{
+	//list A C M E
+	char Idnumber[6];
+	int C;
+	int M;
+	int E;
+	int A;
+};
+
+student grades[2000];
+char checker[2000][6];
+char display[4] = { 'A','C','M','E' };
+
+int main()
+{
+
+
+
+	return 0;
+}
