775d27f2c255cd16ef4d134db5d66af2b97f575e
WangChengtao <821997225@qq.com>
2018-09-04 21:09:40 +0800

    add 1025 but use map ，still have some problem。i want to change in struct and vector

---
+++ PAT_Test/1025 PAT Ranking/PAT_Ranking/PAT_Ranking/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+
+using namespace std;
+
+bool compare(map<pair(string,int)> s1, map<string, int> s2)
+{
+	return s1 >
+}
+
+
+
+int main()
+{
+	
+	int N = 0;
+	cin >> N;
+	vector<int> K(N);
+	map<string, int> message;
+	for(int i=0;i<N;i++)
+	{
+		cin >> K[i];
+		for(int j=0;j<K[i];j++)
+		{
+			string tempstr;
+			int tempint;
+			cin >> tempstr >> tempint;
+		}
+	}
+
+	cout << message.size() << endl;
+
+
+
+
+
+	return 0;
+}
