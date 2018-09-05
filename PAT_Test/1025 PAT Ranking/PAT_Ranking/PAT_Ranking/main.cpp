4f4d97b24f6085b89d6019fa760fbe11b32acbc3
WangChengtao <821997225@qq.com>
2018-09-05 11:26:59 +0800

    finish 1026

--- PAT_Test/1025 PAT Ranking/PAT_Ranking/PAT_Ranking/main.cpp
+++ PAT_Test/1025 PAT Ranking/PAT_Ranking/PAT_Ranking/main.cpp
@@ -1,40 +1,38 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<string>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
-
-bool compare(map<pair(string,int)> s1, map<string, int> s2)
-{
-	return s1 >
+struct student {
+	long long int no;
+	int score, finrank, loca, locarank;
+};
+bool cmp1(student a, student b) {
+	return a.score != b.score ? a.score > b.score : a.no < b.no;
 }
-
-
-
-int main()
-{
-	
-	int N = 0;
-	cin >> N;
-	vector<int> K(N);
-	map<string, int> message;
-	for(int i=0;i<N;i++)
-	{
-		cin >> K[i];
-		for(int j=0;j<K[i];j++)
-		{
-			string tempstr;
-			int tempint;
-			cin >> tempstr >> tempint;
+int main() {
+	int n, m;
+	scanf("%d", &n);
+	vector<student> fin;
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &m);
+		vector<student> v(m);
+		for (int j = 0; j < m; j++) {
+			scanf("%lld %d", &v[j].no, &v[j].score);
+			v[j].loca = i;
+		}
+		sort(v.begin(), v.end(), cmp1);
+		v[0].locarank = 1;
+		fin.push_back(v[0]);
+		for (int j = 1; j < m; j++) {
+			v[j].locarank = (v[j].score == v[j - 1].score) ? (v[j - 1].locarank) : (j + 1);
+			fin.push_back(v[j]);
 		}
 	}
-
-	cout << message.size() << endl;
-
-
-
-
-
+	sort(fin.begin(), fin.end(), cmp1);
+	fin[0].finrank = 1;
+	for (int j = 1; j < fin.size(); j++)
+		fin[j].finrank = (fin[j].score == fin[j - 1].score) ? (fin[j - 1].finrank) : (j + 1);
+	printf("%d\n", fin.size());
+	for (int i = 0; i < fin.size(); i++)
+		printf("%013lld %d %d %d\n", fin[i].no, fin[i].finrank, fin[i].loca, fin[i].locarank);
 	return 0;
 }
